a983bf0f15051fb8c386dc80ae0a63b0ec2bdfb1
Sergey Napiev <napiev@mail.ru>
2023-12-14 17:16:33 +0600

    frontend

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./AboutMe.css";
+
+function AboutMe() {
+    return (
+        <section className="me">
+            <h2 className="me__title">Студент</h2>
+            <p className="me__name">Сергей</p>
+            <p className="me__description">Я родился в Москве, живу в Кокшетау, закончил факультет машиностроительных технологий в МГТУ им.Н.Э.Баумана. У меня есть любимая жена. Я люблю слушать музыку, а ещё увлекаюсь кроссфитом. Недавно начал кодить. С 2017 по 2022 года работал в компании «Лабиринт». Сейчас работаю в ТОО КАИК.</p>
+        </section>
+    )
+}
+
+export default AboutMe;
